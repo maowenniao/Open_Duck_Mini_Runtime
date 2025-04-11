fd056eed697a333f1df5a652ada3bcd6860cc379
maowenniao <maowenniao@163.com>
2025-04-11 08:34:34 +0800

    Update v2_rl_walk_mujoco.py

--- scripts/v2_rl_walk_mujoco.py
+++ scripts/v2_rl_walk_mujoco.py
@@ -39,7 +39,7 @@ class RLWalk:
         self.policy = OnnxInfer(self.onnx_model_path, awd=True)
 
         self.num_dofs = 14
-        self.max_motor_velocity = 5.24  # rad/s
+        self.max_motor_velocity = 4.71  # rad/s
 
         # Control
         self.control_freq = control_freq
@@ -84,7 +84,7 @@ class RLWalk:
         self.imu = Imu(
             sampling_freq=int(self.control_freq),
             user_pitch_bias=self.pitch_bias,
-            upside_down=False,
+            upside_down=True,
         )
 
         self.eyes = Eyes()
